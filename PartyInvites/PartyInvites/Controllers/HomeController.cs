9f389f4abcef09facc1398a33f93b7e5e46e7421
Babak Aghili <babakaghili@icloud.com>
2014-06-18 22:22:00 -0500

    End of Chapter Two
    Except  emailing.

---
+++ PartyInvites/PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PartyInvites.Models;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ViewResult Index()
+        {
+            DateTime dt = DateTime.Now;
+            ViewBag.Greeting = dt.Hour > 12 ? "Good Evening Commisioner!" : "Morning You!";
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                //TODO: Email responses to party organizer.
+                return View("Thanks",guestResponse);
+            }
+            else
+            {
+                // there is some validation error so return them to fix it
+                return View();
+            }
+    
+        }
+    }
+}
---
+++ PartyInvites/PartyInvites/Models/GuestResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PartyInvites.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage = "Please enter your damn name") ]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Please enter your fucking email")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Please enter your phone")]
+        public string Phone { get; set; }
+        [Required(ErrorMessage = "Dude! Will you come or not?!")]
+        public bool? WillAttend { get; set; }
+    }
+}
